fef47fdaba4905d156f7384b8f8d483741549263
Hiroki Chiyoda <contact@arafipro.com>
2024-11-10 16:12:15 +0900

    feat: 全投稿を取得するGET APIを作成
    npx prisma generateを実行
    bun i @prisma/adapter-d1を実行

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,3 +1,6 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
@@ -5,10 +8,12 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
+const adapter = new PrismaD1(getRequestContext().env.DB);
+const prisma = new PrismaClient({ adapter });
+
+app.get("/", async (c) => {
+  const posts = await prisma.post.findMany();
+  return c.json(posts);
 });
 
 export const GET = handle(app);
